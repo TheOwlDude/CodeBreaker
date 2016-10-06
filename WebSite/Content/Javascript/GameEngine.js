be5c6d20b9b6475ffe22a0f13238dacbf68fb527
reece cole <reece cole>
2016-10-05 23:12:58 -0700

    beginning of migration of the game engine to javascript

---
+++ WebSite/Content/Javascript/GameEngine.js
@@ -0,0 +1,94 @@
+﻿function RemoveBlackPositions(code, guess) {
+    var newArrayCounter = 0;
+    var newCode = [];
+    var newGuess = [];
+    var i;
+    for (i = 0; i < code.length; ++i) {
+        if (code[i] != guess[i]) {
+            newCode[newArrayCounter] = code[i];
+            newGuess[newArrayCounter] = guess[i];
+            ++newArrayCounter;
+        }
+    }
+
+    return { NewCode: newCode, NewGuess: newGuess };
+}
+
+
+
+function RemoveWhitePositions(code, guess) {
+    
+    var newCodeCounter = 0;
+    var newCode = [];
+
+    var cloneGuess = guess.slice(0);
+    var i;
+    for(i = 0; i < code.length; ++i) {
+        var foundMatch = false;
+        for (j = 0; j < cloneGuess.length; ++j) {
+            if (code[i] == cloneGuess[j]) {
+                foundMatch = true;
+                cloneGuess.splice(j, 1);
+                break;
+            }
+        }
+        if (!foundMatch) newCode[newCodeCounter++] = code[i];
+    }
+
+    return { NewCode: newCode, NewGuess: cloneGuess };
+}
+
+function CalculateGuessResult(code, guess) {
+    var sansBlacks = RemoveBlackPositions(code, guess);
+    var sansWhites = RemoveWhitePositions(code, guess);
+    return { BlackCount: code.length - sansBlacks.NewCode.length, WhiteCount: sansBlacks.NewCode.length - sansWhites.NewCode.length };
+}
+
+function ResultIsConsistentWithCode(code, guess, compareResult) {
+    var guessResult = CalculateGuessResult(code, guess);
+    return guessResult.BlackCount == compareResult.BlackCount && guessResult.WhiteCount == compareResult.WhiteCount;
+}
+
+
+function AreResultsConsistentWithCode(code, guessResults) {
+    var i;
+    for (i = 0; i < guessResults.length; ++i) {
+        if (!ResultIsConsistentWithCode(code, guessResults[i].Guess, guessResults[i].Result)) return false;
+    }
+    return true;
+}
+
+
+function AddCompleteCodeToList(codeList, code, codeLength, colorCount) {
+    if (code.length == codeLength) codeList.push(code);
+    else {
+        var i;
+        for (i = 0; i < colorCount; ++i) {
+            var newColor = [i];
+            AddCompleteCodeToList(codeList, code.concat(newColor), codeLength, colorCount);
+        }
+    }
+}
+
+
+function StringifyCode(code) {
+    var stringification = "[";
+    var i;
+    for (i = 0; i < code.length; ++i) {
+        if (i != 0) stringification += ",";
+        stringification += code[i];
+    }
+    return stringification += "]";
+}
+
+
+function CodesConsistentWithGuessResults(codeLength, colorCount, guessResults) {
+    var allCodes = [];
+    AddCompleteCodeToList(allCodes, [], codeLength, colorCount);    
+    return allCodes.filter(
+        function (element, index, array) {
+            return AreResultsConsistentWithCode(element, guessResults);
+        }
+    );
+    
+}
---
+++ WebSite/Content/Javascript/GameEngineTests.js
@@ -0,0 +1,366 @@
+﻿QUnit.test(
+    "RemoveBlackPositions_NoMatches_NoRemovals",
+    function (assert) {
+        var code = [1, 1, 1, 1];
+        var guess = [2, 2, 2, 2];
+
+        var result = RemoveBlackPositions(code, guess);
+
+        assert.equal(result.NewCode.length, 4);
+        assert.equal(result.NewGuess.length, 4);
+        for (i = 0; i < 4; ++i) {
+            assert.equal(result.NewCode[i], 1);
+            assert.equal(result.NewGuess[i], 2);
+        }
+    }
+);
+
+QUnit.test(
+    "RemoveBlackPositions_RemoveMatchFromMiddle",
+    function (assert) {
+        var code = [6, 1, 7, 8];
+        var guess = [2, 1, 3, 4];
+
+        var result = RemoveBlackPositions(code, guess);
+
+        assert.equal(result.NewCode.length, 3);
+        assert.equal(result.NewGuess.length, 3);
+
+        assert.equal(result.NewCode[0], 6);
+        assert.equal(result.NewGuess[0], 2);
+
+        assert.equal(result.NewCode[1], 7);
+        assert.equal(result.NewGuess[1], 3);
+
+        assert.equal(result.NewCode[2], 8);
+        assert.equal(result.NewGuess[2], 4);
+    }
+);
+
+QUnit.test(
+    "RemoveBlackPositions_RemoveAll",
+    function (assert) {
+        var code = [6, 1, 7, 8];
+        var guess = [6, 1, 7, 8];
+
+        var result = RemoveBlackPositions(code, guess);
+
+        assert.equal(result.NewCode.length, 0);
+        assert.equal(result.NewGuess.length, 0);
+    }
+);
+
+
+
+QUnit.test(
+    "RemoveWhitePositions_RemoveNone",
+    function (assert) {
+       var code = [4, 5, 6];
+       var guess = [1, 2, 3];
+
+       var result = RemoveWhitePositions(code, guess);
+
+       assert.equal(result.NewCode.length, 3);
+       assert.equal(result.NewGuess.length, 3);
+
+       assert.equal(result.NewCode[0], 4);
+       assert.equal(result.NewCode[1], 5);
+       assert.equal(result.NewCode[2], 6);
+       assert.equal(result.NewGuess[0], 1);
+       assert.equal(result.NewGuess[1], 2);
+       assert.equal(result.NewGuess[2], 3);
+    }
+);
+
+
+QUnit.test(
+    "RemoveWhitePositions_Remove2",
+    function (assert) {
+        var code = [4, 5, 6];
+        var guess = [6, 2, 4];
+
+        var result = RemoveWhitePositions(code, guess);
+
+        assert.equal(result.NewCode.length, 1);
+        assert.equal(result.NewGuess.length, 1);
+
+        assert.equal(result.NewCode[0], 5);
+        assert.equal(result.NewGuess[0], 2);
+    }
+);
+
+QUnit.test(
+    "RemoveWhitePositions_RemoveAll",
+    function (assert) {
+        var code = [4, 5, 6];
+        var guess = [6, 4, 5];
+
+        var result = RemoveWhitePositions(code, guess);
+
+        assert.equal(result.NewCode.length, 0);
+        assert.equal(result.NewGuess.length, 0);
+    }
+);
+
+
+QUnit.test(
+    "CalculateGuessResult_NoMatches",
+    function (assert) {
+        var code = [1, 2, 3, 4];
+        var guess = [9, 8, 7, 6];
+
+        var result = CalculateGuessResult(code, guess);
+        assert.equal(result.BlackCount, 0);
+        assert.equal(result.WhiteCount, 0);
+    }
+);
+
+QUnit.test(
+    "CalculateGuessResult_1_White",
+    function (assert) {
+        var code = [1, 2, 3, 4];
+        var guess = [4, 8, 7, 6];
+
+        var result = CalculateGuessResult(code, guess);
+        assert.equal(result.BlackCount, 0);
+        assert.equal(result.WhiteCount, 1);
+    }
+);
+
+QUnit.test(
+    "CalculateGuessResult_Multiple_Whites",
+    function (assert) {
+        var code = [1, 2, 3, 4];
+        var guess = [4, 3, 2, 6];
+
+        var result = CalculateGuessResult(code, guess);
+        assert.equal(result.BlackCount, 0);
+        assert.equal(result.WhiteCount, 3);
+    }
+);
+
+QUnit.test(
+    "CalculateGuessResult_All_Whites",
+    function (assert) {
+        var code = [1, 2, 3, 4];
+        var guess = [4, 3, 2, 1];
+
+        var result = CalculateGuessResult(code, guess);
+        assert.equal(result.BlackCount, 0);
+        assert.equal(result.WhiteCount, 4);
+    }
+);
+
+
+QUnit.test(
+    "CalculateGuessResult_1_Black",
+    function (assert) {
+        var code = [1, 2, 3, 4];
+        var guess = [9, 2, 8, 7];
+
+        var result = CalculateGuessResult(code, guess);
+        assert.equal(result.BlackCount, 1);
+        assert.equal(result.WhiteCount, 0);
+    }
+);
+
+QUnit.test(
+    "CalculateGuessResult_Multiple_Blacks",
+    function (assert) {
+        var code = [1, 2, 3, 4];
+        var guess = [9, 2, 8, 4];
+
+        var result = CalculateGuessResult(code, guess);
+        assert.equal(result.BlackCount, 2);
+        assert.equal(result.WhiteCount, 0);
+    }
+);
+
+QUnit.test(
+    "CalculateGuessResult_All_Blacks",
+    function (assert) {
+        var code = [1, 2, 3, 4];
+        var guess = [1, 2, 3, 4];
+
+        var result = CalculateGuessResult(code, guess);
+        assert.equal(result.BlackCount, 4);
+        assert.equal(result.WhiteCount, 0);
+    }
+);
+
+QUnit.test(
+    "CalculateGuessResult_Some_Whites_And_Blacks",
+    function (assert) {
+        var code = [1, 2, 3, 4];
+        var guess = [1, 2, 4, 8];
+
+        var result = CalculateGuessResult(code, guess);
+        assert.equal(result.BlackCount, 2);
+        assert.equal(result.WhiteCount, 1);
+    }
+);
+
+QUnit.test(
+    "CalculateGuessResult_All_Whites_And_Blacks",
+    function (assert) {
+        var code = [1, 2, 3, 4];
+        var guess = [1, 2, 4, 3];
+
+        var result = CalculateGuessResult(code, guess);
+        assert.equal(result.BlackCount, 2);
+        assert.equal(result.WhiteCount, 2);
+    }
+);
+
+QUnit.test(
+    "ResultIsConsistentWithCode_Neither_Match",
+    function (assert) {
+        var code = [1, 2, 3, 4];
+        var guess = [1, 3, 7, 8];
+        var result = { BlackCount: 2, WhiteCount: 0 };
+        assert.notOk(ResultIsConsistentWithCode(code, guess, result));
+    }
+);
+
+QUnit.test(
+    "ResultIsConsistentWithCode_Just_White_Matches",
+    function (assert) {
+        var code = [1, 2, 3, 4];
+        var guess = [1, 3, 7, 8];
+        var result = { BlackCount: 0, WhiteCount: 1 };
+        assert.notOk(ResultIsConsistentWithCode(code, guess, result));
+    }
+);
+
+QUnit.test(
+    "ResultIsConsistentWithCode_Just_Black_Matches",
+    function (assert) {
+        var code = [1, 2, 3, 4];
+        var guess = [1, 3, 7, 8];
+        var result = { BlackCount: 1, WhiteCount: 3 };
+        assert.notOk(ResultIsConsistentWithCode(code, guess, result));
+    }
+);
+
+QUnit.test(
+    "ResultIsConsistentWithCode_Both_Match",
+    function (assert) {
+        var code = [1, 2, 3, 4];
+        var guess = [1, 3, 7, 8];
+        var result = { BlackCount: 1, WhiteCount: 1 };
+        assert.ok(ResultIsConsistentWithCode(code, guess, result));
+    }
+);
+
+QUnit.test(
+    "AreResultsConsistentWithCode_First_Not_Consistent",
+    function (assert) {
+
+        assert.notOk(
+            AreResultsConsistentWithCode(
+                [1, 2, 3, 4],
+                [
+                    {
+                        Guess: [1, 3, 6, 7],
+                        Result: {BlackCount: 1, WhiteCount: 2}
+                    },
+                    {
+                        Guess: [1, 3, 6, 7],
+                        Result: { BlackCount: 1, WhiteCount: 1 }
+                    },
+                ]
+            )
+        );        
+    }
+);
+
+QUnit.test(
+    "AreResultsConsistentWithCode_Last_Not_Consistent",
+    function (assert) {
+
+        assert.notOk(
+            AreResultsConsistentWithCode(
+                [1, 2, 3, 4],
+                [
+                    {
+                        Guess: [1, 3, 6, 7],
+                        Result: { BlackCount: 1, WhiteCount: 1 }
+                    },
+                    {
+                        Guess: [1, 3, 6, 7],
+                        Result: { BlackCount: 1, WhiteCount: 2 }
+                    },
+                ]
+            )
+        );
+    }
+);
+
+QUnit.test(
+    "AreResultsConsistentWithCode_Both_Consistent",
+    function (assert) {
+
+        assert.ok(
+            AreResultsConsistentWithCode(
+                [1, 2, 3, 4],
+                [
+                    {
+                        Guess: [1, 3, 6, 7],
+                        Result: { BlackCount: 1, WhiteCount: 1 }
+                    },
+                    {
+                        Guess: [1, 3, 6, 7],
+                        Result: { BlackCount: 1, WhiteCount: 1 }
+                    },
+                ]
+            )
+        );
+    }
+);
+
+QUnit.test(
+    "AddCompleteCodeToList_DumpResults",
+    function (assert) {
+        assert.expect(0);
+        var completeCodeList = [];        
+        AddCompleteCodeToList(completeCodeList, [], 3, 3);
+        
+        var i;
+        for (i = 0; i < completeCodeList.length; ++i) {
+            console.log(StringifyCode(completeCodeList[i]));
+        }
+    }
+);
+
+
+QUnit.test(
+    "CodesConsistentWithGuessResults_DumpResults",
+    function (assert) {
+        assert.expect(0);
+        CodesConsistentWithGuessResults(4, 4,
+            [
+                {
+                    Guess: [3, 2, 3, 1],
+                    Result: {
+                        BlackCount: 0,
+                        WhiteCount: 3
+                    }
+                },
+                {
+                    Guess: [0, 2, 1, 0],
+                    Result: {
+                        BlackCount: 1,
+                        WhiteCount: 2
+                    }
+                }
+            ]
+
+        ).forEach(
+            function (currentValue, index, array) {
+                console.log(StringifyCode(currentValue));
+            }
+            
+        );
+    }
+);
+
